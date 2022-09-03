43d391e26862627c2101c77891c96f66638d9f70
Nandan-unni <asnqln@gmail.com>
2022-09-03 10:47:36 +0530

    feat: initated project using NextJs and Tailwind

---
+++ src/pages/_app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/globals.css";
+
+const ShotV = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default ShotV;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index = () => {
+  return <div className="text-5xl text-themeColor">Index</div>;
+};
+
+export default Index;
